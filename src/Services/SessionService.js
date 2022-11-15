bfda7a5cf26c6a6483dc1c881067c6771cfbed44
sylla <ibrahisyllac196@gmail.com>
2022-11-15 12:11:45 +0000

    FEATURE(role): add the differentre role in the website

--- src/Services/SessionService.js
+++ src/Services/SessionService.js
@@ -1,14 +1,26 @@
 const SessionService = {
-    async Login(email, password) {
-       localStorage.setItem("accessToken", "test");
-       localStorage.setItem("user", { name: "sylla", lastName: "ibrahim" });
-
-    },
-    async Logout() {
-        localStorage.removeItem("accessToken");
-       localStorage.removeItem("user");
-
-    },
+  async Login(email, password) {
+    localStorage.setItem("accessToken", "test");
+    if (email === "lender@gmail.com")
+      localStorage.setItem(
+        "user",
+        JSON.stringify({ name: "sylla", lastName: "ibrahim", role: "lender" })
+      );
+    else if (email === "borrower@gmail.com")
+      localStorage.setItem(
+        "user",
+        JSON.stringify({ name: "sylla", lastName: "ibrahim", role: "borrower" })
+      );
+    else if (email === "admin@gmail.com")
+      localStorage.setItem(
+        "user",
+        JSON.stringify({ name: "sylla", lastName: "ibrahim", role: "admin" })
+      );
+  },
+  async Logout() {
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("user");
+  },
 };
 
 export default SessionService;
