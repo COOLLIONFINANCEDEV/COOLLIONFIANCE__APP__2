6e075d679fc838b2774addd6f3aab145cde3ddb9
sylla <s67986601@gmail.com>
2022-12-28 22:00:03 +0000

    FEATURE(wallet)

--- src/Pages/Wallet.jsx
+++ src/Pages/Wallet.jsx
@@ -1,7 +1,22 @@
+import { useTheme } from "@emotion/react";
+import { Box } from "@mui/material";
 import React from "react";
 
 const Wallet = () => {
-  return <div>Wallet</div>;
+  const { palette, width } = useTheme();
+  const WalletStyle = {
+    margin: "0 auto 0 auto",
+    marginTop: { xs: "4vh", md: "10vh" },
+    width: width,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "flex-start",
+  };
+  return (
+    <Box sx={WalletStyle}>
+      <Box></Box>
+    </Box>
+  );
 };
 
 export default Wallet;
